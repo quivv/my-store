c7d493124d07981330f42cc0a9a244b5adae925f
qui <quivv.dtp@gmail.com>
2022-07-25 06:08:06 +0700

    qui add

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {About, Contact, Product, Home, Header, Footer, ProductDetail} from './components';
+import {About, Contact, Product, Home, Header, Footer, ProductDetail, Cart} from './components';
 import {Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -11,6 +11,7 @@ function App() {
         <Route path='/' element = {<Home/>}/>
         <Route path='/products' element={<Product/>}/>
         <Route path='/products/:id' element={<ProductDetail/>}/>
+        <Route path='/cart' element={<Cart/>}/>
         <Route path='/about' element={<About/>}/>
         <Route path='/contact' element={<Contact/>}/>
       </Routes>
--- src/hooks/useCartContext.ts
+++ src/hooks/useCartContext.ts
@@ -3,13 +3,11 @@ import { useAppSelector } from "./reduxHooks";
 
 export interface CartContext {
   cardList: ProductType[];
-  cartFirst: ProductType;
 }
 
 export function useCartContext(): CartContext {
   const cart = useAppSelector((state) => state.handleCart);
   return {
     cardList: cart,
-    cartFirst: cart?.[0],
   };
 }
